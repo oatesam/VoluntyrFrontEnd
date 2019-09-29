d384bc4f9f0229a6e56c68a889c35ef397644027
Charlie <Charlie.frank72@gmail.com>
2019-09-29 14:30:29 -0400

    temp

--- voluntyrFrontend/src/app/app-routing.module.ts
+++ voluntyrFrontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {LoginComponent} from './login/login.component';
 
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent },
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- voluntyrFrontend/src/app/app.module.ts
+++ voluntyrFrontend/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    LoginComponent,
     AppComponent,
     LandingPageComponent,
     PageNotFoundComponent,
