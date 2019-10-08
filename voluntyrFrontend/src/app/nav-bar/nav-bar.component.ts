01e58a1fde5e87eeeb88237989547094c634246d
imjuststackoverflow <samoateswl@gmail.com>
2019-10-08 19:36:02 -0400

    login saves token locally

--- voluntyrFrontend/src/app/nav-bar/nav-bar.component.ts
+++ voluntyrFrontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '@app/_services/authentication.service';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
   selector: 'app-nav-bar',
