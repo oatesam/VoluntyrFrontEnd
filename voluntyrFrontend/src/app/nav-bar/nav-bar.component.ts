8310d40424704da842003679a8bdfdda147494fa
imjuststackoverflow <samoateswl@gmail.com>
2019-10-10 16:31:22 -0400

    changed email-input to parent login/register

--- voluntyrFrontend/src/app/nav-bar/nav-bar.component.ts
+++ voluntyrFrontend/src/app/nav-bar/nav-bar.component.ts
@@ -13,16 +13,6 @@ export class NavBarComponent implements OnInit {
   public currentUser = this.authService.currentUserValue;
 
   ngOnInit() {
-    this.authService.login(this.currentUser.username, this.currentUser.password).subscribe(
-      (data) => {
-        console.log(data);
-        this.ngOnInit();
-      },
-      error => {
-        console.log('Error');
-      }
-    );
-
     if(this.authService.currentUserValue){
       this.showLogin = false;
     } else {
--- voluntyrFrontend/src/app/register/register.component.ts
+++ voluntyrFrontend/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {AccountsService} from '../_services/accounts.service';
 import {Router, ActivatedRoute} from '@angular/router';
@@ -10,16 +10,22 @@ import {Router, ActivatedRoute} from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private accountService: AccountsService, private router: Router, private route: ActivatedRoute) {
-  }
+  @Input() email: string;
+
+  constructor(private accountService: AccountsService,
+              private router: Router,
+              private route: ActivatedRoute
+  ) {  }
 
   public showOrg: boolean = false;
   public buttonName: any = 'Volunteer';
-  firstname = new FormControl('Sam');
-  lastname = new FormControl('Fake');
-  email = new FormControl('samfake@gmail.com');
-  password = new FormControl('samfake');
-  birthday = new FormControl('1998-06-12')
+  public oppReg: any = 'an Organization';
+  public curReg: any = 'a Volunteer'
+  firstname = new FormControl('');
+  lastname = new FormControl('');
+  emailControl = new FormControl('');
+  password = new FormControl('');
+  birthday = new FormControl('')
   orgname = new FormControl('')
   address = new FormControl('');
   phonenumber = new FormControl('');
@@ -28,35 +34,40 @@ export class RegisterComponent implements OnInit {
     this.showOrg = !this.showOrg;
     if (this.showOrg) {
       this.buttonName = 'Organization';
+      this.oppReg = 'a Volunteer';
     } else {
       this.buttonName = 'Volunteer';
+      this.curReg = 'an Organization';
     }
   }
 
   ngOnInit() {
+    this.emailControl.setValue(this.email);
   }
 
   verifyVolunteerRegistration() {
-    this.accountService.registerVolunteer(this.firstname.value, this.lastname.value, this.email.value, this.password.value, this.birthday.value).subscribe(
+    this.accountService.registerVolunteer(this.firstname.value, this.lastname.value, this.emailControl.value, this.password.value, this.birthday.value).subscribe(
       resp => {
-        if (resp === 202) {
+        console.log(resp)
+        if (resp['status'] === 202) {
           console.log(resp);
-          this.router.navigateByUrl('../../login');
-        } else if (resp === 204) {
+          this.router.navigateByUrl('login');
+        } else if (resp['status'] === 204) {
           console.log(resp);
-          this.router.navigateByUrl('');
+          this.router.navigateByUrl('register');
         }
       }
     );
   }
 
   verifyOrganizationRegistration() {
-    this.accountService.registerOrganization(this.orgname.value, this.email.value, this.password.value, this.address.value, this.phonenumber.value).subscribe(
+    this.accountService.registerOrganization(this.orgname.value, this.emailControl.value, this.password.value, this.address.value, this.phonenumber.value).subscribe(
       resp => {
-        if (resp === 202) {
-          console.log(resp);
-          this.router.navigateByUrl('../../login');
-        } else if (resp === 204) {
+        console.log('is this thing working?')
+        if (resp['status'] === 409) {
+          console.log('got into status');
+          this.router.navigateByUrl('login');
+        } else if (resp['status'] === 204) {
           console.log(resp);
           this.router.navigateByUrl('');
         }
