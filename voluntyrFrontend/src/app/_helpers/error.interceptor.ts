20ac410a1b9074881861b7f09993308cb3bf75f2
imjuststackoverflow <samoateswl@gmail.com>
2019-10-11 12:32:25 -0400

    Login now updates correctly, error and jwt interceptors function

--- voluntyrFrontend/src/app/_helpers/error.interceptor.ts
+++ voluntyrFrontend/src/app/_helpers/error.interceptor.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
 import { AuthenticationService } from '../_services/authentication.service';
 import {AlertService} from "@app/_services/alert.service";
 
