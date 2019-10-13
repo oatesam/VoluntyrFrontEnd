54f0e9e312e2dd1fd419c8371d5ade94d3d18900
Chenxu <wang542@iu.edu>
2019-10-13 11:27:15 -0400

    complete story-7

--- voluntyrFrontend/src/app/_helpers/events.service.ts
+++ voluntyrFrontend/src/app/_helpers/events.service.ts
@@ -7,10 +7,11 @@ import { Observable } from "rxjs";
 })
 export class EventsService {
   private baseurl = "http://localhost:8000/api/events/";
+  private token = JSON.parse(localStorage.getItem("currentUser")).access;
   constructor(private httpClient: HttpClient) {}
   httpOptions = {
     headers: new HttpHeaders({
-      Authorization: ` Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcwNjEwMjE1LCJqdGkiOiJiOTEwMjA1OTEzNmU0YWZiODJhOGZkYTg2ZmI0YjYyZCIsInVzZXJfaWQiOjIsInNjb3BlIjoib3JnYW5pemF0aW9uIn0.apFTYEkmeIWhmhJR66xJZ8kj3ob16nRiq8AvbLkNOCE`
+      Authorization: ` Bearer ${this.token}`
     })
   };
   getEventInfo(): Observable<event[]> {
--- voluntyrFrontend/src/app/_helpers/organization.service.ts
+++ voluntyrFrontend/src/app/_helpers/organization.service.ts
@@ -7,11 +7,11 @@ import { Observable } from "rxjs";
 })
 export class OrganizationService {
   private baseurl = "http://localhost:8000/api/organization/";
-  s;
+  private token = JSON.parse(localStorage.getItem("currentUser")).access;
   constructor(private httpClient: HttpClient) {}
   httpOptions = {
     headers: new HttpHeaders({
-      Authorization: ` Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcwNjEwMjE1LCJqdGkiOiJiOTEwMjA1OTEzNmU0YWZiODJhOGZkYTg2ZmI0YjYyZCIsInVzZXJfaWQiOjIsInNjb3BlIjoib3JnYW5pemF0aW9uIn0.apFTYEkmeIWhmhJR66xJZ8kj3ob16nRiq8AvbLkNOCE`
+      Authorization: ` Bearer ${this.token}`
     })
   };
   getOrganizationInfo(): Observable<organization> {
