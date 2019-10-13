e855fa930c12d35408532f6c269408b5b57f0710
Chenxu <wang542@iu.edu>
2019-10-13 12:49:48 -0400

    fix

--- voluntyrFrontend/src/app/app-routing.module.ts
+++ voluntyrFrontend/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
@@ -8,7 +7,7 @@ import { RegisterComponent } from "./register/register.component";
 import { EmailInputComponent } from "./email-input/email-input.component";
 import { OrganizerDashboardComponent } from "./organizer-dashboard/organizer-dashboard.component";
 import { AuthGuard } from "./_helpers/auth.guard";
-import { VolunteerDashboardComponent } from './volunteer-dashboard/volunteer-dashboard.component';
+import { VolunteerDashboardComponent } from "./volunteer-dashboard/volunteer-dashboard.component";
 
 const routes: Routes = [
   { path: "", component: LandingPageComponent },
@@ -24,7 +23,7 @@ const routes: Routes = [
     component: OrganizerDashboardComponent,
     canActivate: [AuthGuard]
   },
-  {path: 'dashboard/volunteer', component: VolunteerDashboardComponent},
+  { path: "dashboard/volunteer", component: VolunteerDashboardComponent },
   // PageNotFound should always be last in routing, otherwise it will overtake others
   { path: "**", component: PageNotFoundComponent }
 ];
