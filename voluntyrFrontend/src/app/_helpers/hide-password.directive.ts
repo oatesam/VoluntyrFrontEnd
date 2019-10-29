90654048e9c28cd096b7bd1faeb40874188667e1
imjuststackoverflow <samoateswl@gmail.com>
2019-10-29 16:27:43 -0400

    landing page tentatively complete

--- voluntyrFrontend/src/app/_helpers/hide-password.directive.ts
+++ voluntyrFrontend/src/app/_helpers/hide-password.directive.ts
@@ -2,7 +2,8 @@ import {Directive, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appHidePassword]'
-})
+}
+)
 export class HidePasswordDirective {
   private _shown = false;
   constructor(private el: ElementRef, private render: Renderer2) {
@@ -24,11 +25,7 @@ export class HidePasswordDirective {
     const parent = this.el.nativeElement.parentNode;
     const span = document.createElement('p');
     span.innerHTML = 'Show Password';
-    span.setAttribute('class', 'password-toggle');
-    span.setAttribute('padding-bottom', '0px');
-    span.setAttribute('padding-top', '5px');
-    span.setAttribute('display', 'block');
-    span.setAttribute('font-size','80px');
+    span.style.color = 'white';
     span.addEventListener('click', (event) => {
       this.toggle(span);
     });
