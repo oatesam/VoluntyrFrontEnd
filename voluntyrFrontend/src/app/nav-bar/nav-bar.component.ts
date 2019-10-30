b9b650f86d6ebaeb504afbf6b730c67f71a51f72
Chenxu <wang542@iu.edu>
2019-10-29 20:59:46 -0400

    added conditional rendering of navbar

--- voluntyrFrontend/src/app/nav-bar/nav-bar.component.ts
+++ voluntyrFrontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../_services/authentication.service';
-import {DataService} from '@app/_services/data.service';
-import {Router} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../_services/authentication.service";
+import { DataService } from "@app/_services/data.service";
+import { Router } from "@angular/router";
+import { decode } from "punycode";
+import { decode as decoder } from "jwt-decode";
 
 // TODO: Filter by scope of logged in user
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.css"]
 })
 export class NavBarComponent implements OnInit {
+  showVolunteer: Boolean = false;
+  showOrganization: Boolean = false;
 
-  constructor(private authService: AuthenticationService,
-              private router: Router) {}
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
   public currentUser = this.authService.currentUserValue;
   public logged: boolean;
+
+  scopeRender() {
+    var decodedToken = decoder(
+      JSON.parse(localStorage.getItem("currentUser")).access
+    );
+    var tokenScope = decodedToken.scope;
+    if (tokenScope === "Volunteer") {
+      this.showVolunteer = true;
+      this.showOrganization = false;
+    } else if (tokenScope === "Organization") {
+      this.showOrganization = true;
+      this.showVolunteer = true;
+    }
+  }
 
   ngOnInit() {
     if (this.authService.currentUserValue) {
@@ -23,6 +43,7 @@ export class NavBarComponent implements OnInit {
     } else {
       this.logged = false;
     }
+    this.scopeRender();
     this.authService.getLogged.subscribe(name => this.changeLog(name));
   }
 
@@ -34,6 +55,4 @@ export class NavBarComponent implements OnInit {
     this.authService.logout();
     window.location.reload();
   }
-
-
 }
