45bacfca702ddb000dbb3b9b8a1b07f1c58ab071
Charlie <Charlie.frank72@gmail.com>
2019-11-01 15:56:01 -0400

    Finished frontend. Waiting for Nikhil's story to link to this component

--- voluntyrFrontend/src/app/app.module.ts
+++ voluntyrFrontend/src/app/app.module.ts
@@ -29,14 +29,13 @@ import {VolunteerEventComponent} from '@app/volunteer-event/volunteer-event.comp
 import {VolunteerEventSignupComponent} from '@app/volunteer-event-signup/volunteer-event-signup.component';
 import {NewEventComponent} from '@app/new-event/new-event.component';
 import {MessageVolunteersComponent} from './message-volunteers/message-volunteers.component';
-import {LoginwrapperComponent} from './loginwrapper/loginwrapper.component';
-import {RegisterwrapperComponent} from './registerwrapper/registerwrapper.component';
-import {NgxSpinnerModule} from 'ngx-spinner';
+import {NgxSpinnerModule } from "ngx-spinner";
+import { LoginwrapperComponent } from './loginwrapper/loginwrapper.component';
+import { RegisterwrapperComponent } from './registerwrapper/registerwrapper.component';import {NgxSpinnerModule} from 'ngx-spinner';
 
 import {EditEventComponent} from './edit-event/edit-event.component';
 import {LoginwrapperComponent} from '@app/loginwrapper/loginwrapper.component';
 import {RegisterwrapperComponent} from '@app/registerwrapper/registerwrapper.component';
-
 
 @NgModule({
   declarations: [
