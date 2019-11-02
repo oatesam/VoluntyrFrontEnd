bcd4cc829f196f64f2b066eccbab9404e85d2211
Charlie <Charlie.frank72@gmail.com>
2019-11-02 18:06:18 -0400

    cleaned up event search

--- voluntyrFrontend/src/app/volunteer-event/volunteer-event.component.ts
+++ voluntyrFrontend/src/app/volunteer-event/volunteer-event.component.ts
@@ -5,11 +5,13 @@ import {
   faMapPin,
   faCalendarAlt,
   faHandHoldingHeart,
-  faLandmark
+  faLandmark,
+  faFileAlt
 } from "@fortawesome/free-solid-svg-icons";
 import { HostListener } from "@angular/core";
 import { VolunteerService } from "../_services/volunteer.service";
 import { Event } from "../_models/Event";
+import {EventsService} from '@app/_services/events.service';
 
 @Component({
   selector: "app-volunteer-event",
@@ -18,23 +20,47 @@ import { Event } from "../_models/Event";
 })
 export class VolunteerEventComponent implements OnInit {
   @Input() event: Event;
-  @HostListener("click") onClick() {
-    console.log("clicked!!!!", this.event.id);
-    this.signUpEvent();
-  }
-  token = JSON.parse(localStorage.getItem("currentUser")).access;
-  //declaration of the icons
 
+  token = JSON.parse(localStorage.getItem("currentUser")).access;
+
+  //declaration of the icons
   faBullhorn = faBullhorn;
   faLandmark = faLandmark;
   faClock = faClock;
+  faDescription = faFileAlt;
   faMapPin = faMapPin;
   faCalendarAlt = faCalendarAlt;
   faHandHoldingHeart = faHandHoldingHeart;
-  signUpEvent() {
-    this.VolunteerService.signupEvents(this.token, this.event.id);
-  }
-  constructor(private VolunteerService: VolunteerService) {}
+  signedup: boolean = false;
 
-  ngOnInit() {}
+  constructor(private VolunteerService: VolunteerService, private es: EventsService) {}
+
+  ngOnInit() {
+    this.checkSignup();
+  }
+
+  signUpEvent() {
+    this.VolunteerService.signupEvents(this.token, this.event.id).subscribe(
+      data => {
+        console.log("Success", data);
+        this.checkSignup();
+      },
+      error1 => {
+        console.error(error1);
+        alert("There was a problem registering or unregistering for this event, please try again later.");
+      }
+    );
+  }
+
+  checkSignup() {
+    this.es.checkSignUp(this.event.id).subscribe(
+      data => {
+        console.log(data);
+        this.signedup = data['Signed-up'] == 'true';
+      },
+      error1 => {
+        console.error(error1);
+      }
+    );
+  }
 }
