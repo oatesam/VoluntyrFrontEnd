132c29c4098f6f0c5e40a663c1a676610e93109c
Chenxu <wang542@iu.edu>
2019-11-12 10:32:55 -0500

    rebase with dev

--- voluntyrFrontend/src/app/volunteer-event-signup/volunteer-event-signup.component.ts
+++ voluntyrFrontend/src/app/volunteer-event-signup/volunteer-event-signup.component.ts
@@ -1,29 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { VolunteerService } from '../_services/volunteer.service';
-import { Volunteer } from '../_models/Volunteer';
-import { SearchEvent } from '../_models/SearchEvent';
-import { faUser, faCalendarPlus } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from "@angular/core";
+import { VolunteerService } from "../_services/volunteer.service";
+import { Volunteer } from "../_models/Volunteer";
+import { SearchEvent } from "../_models/SearchEvent";
+import { faUser, faCalendarPlus } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-volunteer-event-signup',
-  templateUrl: './volunteer-event-signup.component.html',
-  styleUrls: ['./volunteer-event-signup.component.css']
+  selector: "app-volunteer-event-signup",
+  templateUrl: "./volunteer-event-signup.component.html",
+  styleUrls: ["./volunteer-event-signup.component.css"]
 })
 export class VolunteerEventSignupComponent implements OnInit {
-
-  constructor(private volunteerService: VolunteerService) { }
+  constructor(private volunteerService: VolunteerService) {}
 
   faUser = faUser;
   faCalendarPlus = faCalendarPlus;
-
+  start_time;
+  end_time;
   public volunteer: Volunteer = new Volunteer();
   public events: SearchEvent[];
 
-  public token = JSON.parse(localStorage.getItem('currentUser')).access;
+  public token = JSON.parse(localStorage.getItem("currentUser")).access;
   ngOnInit() {
     this.getUpcomingEvents();
   }
-
+  public retrieveFilterEvents() {
+    //this.end_time = this.searchForm.controls.endtime.value.toString();
+    this.volunteerService
+      .searchEvents(this.token, this.start_time, this.end_time)
+      .subscribe(
+        data => {
+          this.events = data;
+          console.log(data, "!!!");
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
   private getUpcomingEvents() {
     this.volunteerService.getUpcomingEvents(this.token).subscribe(
       data => {
