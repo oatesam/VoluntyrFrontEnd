1dd9c74df34baa3f7b8600b412711e1a8699b8f9
Chenxu <wang542@iu.edu>
2019-12-03 20:24:03 -0500

    adjusted working version

--- voluntyrFrontend/src/app/_helpers/jwt.interceptor.ts
+++ voluntyrFrontend/src/app/_helpers/jwt.interceptor.ts
@@ -16,58 +16,21 @@ export class JwtInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if (localStorage.getItem("refreshingToken") === "true") {
-      return next.handle(request);
-    } else {
-      const helper = new JwtHelperService();
-      console.log("JWT intercepted, pre intercept = ", request.headers);
-      // add authorization header with jwt token if available
-      let curUser = JSON.parse(localStorage.getItem("currentUser"));
-      console.log("curuser = ", curUser);
-      if (curUser && curUser.access) {
-        let isExpired = helper.isTokenExpired(curUser.access);
-        console.log("before is expired~~~");
-        if (isExpired) {
-          console.log("expireddddddd");
-          localStorage.setItem("refreshingToken", "true");
-          const refreshToken = curUser.refresh;
-          this.authenticationService.refreshToken(refreshToken).subscribe(
-            data => {
-              console.log("inside data!!!!!!!!!!!");
-              var newAccess = data.access;
-              let oldUser = JSON.parse(localStorage.getItem("currentUser"));
-              oldUser.access = newAccess;
-              localStorage.setItem("currentUser", JSON.stringify(oldUser));
-              isExpired = false;
-              curUser = JSON.parse(localStorage.getItem("currentUser"));
-              request = request.clone({
-                setHeaders: {
-                  Authorization: `Bearer ${curUser.access}`
-                }
-              });
-              localStorage.setItem("refreshingToken", "false");
-              return next.handle(request);
-            },
-            error => {
-              return next.handle(request);
-            }
-          );
-        } else {
-          console.log(
-            "JWT Interceptor found curUser, token = ",
-            curUser.access
-          );
-          request = request.clone({
-            setHeaders: {
-              Authorization: `Bearer ${curUser.access}`
-            }
-          });
-          return next.handle(request);
+    const helper = new JwtHelperService();
+    console.log("JWT intercepted, pre intercept = ", request.headers);
+    // add authorization header with jwt token if available
+    let curUser = JSON.parse(localStorage.getItem("currentUser"));
+    console.log("curuser = ", curUser);
+    if (curUser && curUser.access) {
+      console.log("JWT Interceptor found curUser, token = ", curUser.access);
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${curUser.access}`
         }
-      } else {
-        return next.handle(request);
-      }
-      console.log("post intercept = ", request.headers);
+      });
     }
+
+    console.log("post intercept = ", request.headers);
+    return next.handle(request);
   }
 }
