b63c63704106e7ac683d54a737f1ef939bfbbdad
Nikhil B Mankame <nmankame@iu.edu>
2019-12-06 01:00:41 -0500

    Customer Feedback : Final Commit 1

--- voluntyrFrontend/src/app/app-routing.module.ts
+++ voluntyrFrontend/src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ import {AuthGuard} from '@app/_helpers/auth.guard';
 import {RecoverPasswordComponent} from "@app/recover-password/recover-password.component";
 import {ResetPasswordComponent} from "@app/reset-password/reset-password.component";
 import {RateEventsComponent} from '@app/rate-events/rate-events.component';
+import {EmailInputComponent} from "@app/email-input/email-input.component";
 
 const routes: Routes = [
   { path: "", component: LandingPageComponent },
@@ -28,7 +29,7 @@ const routes: Routes = [
   // basically the same as below, just replace 'ExampleComponent'
   // {path: '', component: ExampleComponent, canActivate: [AuthGuard]},
   // will uncomment the above line once AuthGuard is integrated
-  { path: "email-input", component: EmailInputWrapperComponent },
+  { path: "email-input", component: EmailInputComponent },
   { path: "login", component: LoginwrapperComponent },
   { path: "register", component: RegisterwrapperComponent },
   {
