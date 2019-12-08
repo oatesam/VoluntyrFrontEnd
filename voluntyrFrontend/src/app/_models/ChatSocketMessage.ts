e21d618306e8c453e6d2798c7c6275061849734a
Charlie <Charlie.frank72@gmail.com>
2019-12-08 17:36:29 -0500

    Read working

--- voluntyrFrontend/src/app/_models/ChatSocketMessage.ts
+++ voluntyrFrontend/src/app/_models/ChatSocketMessage.ts
@@ -1,12 +1,12 @@
 export class ChatSocketMessage {
   type: string;
-  id: string;
+  id: number;
   room: string;
   status: string;
   sender: string;
   message: string;
 
-  constructor(type: string, id: string, room: string, status: string, sender: string, message: string) {
+  constructor(type: string, id: number, room: string, status: string, sender: string, message: string) {
     this.type = type;
     this.id = id;
     this.room = room;
